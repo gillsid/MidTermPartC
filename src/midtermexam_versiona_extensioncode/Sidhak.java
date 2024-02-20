6e9480de9e80f534ddd274e89e2367be2bb0e13b
Sidhak gill <gillsid@sheridancollege.ca>
2024-02-20 12:32:56 -0500

    2nd commit

---
+++ src/midtermexam_versiona_extensioncode/Sidhak.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *this class is created by sidhak on 20th feb
+ * @author gills
+ */
+public class Sidhak {
+    public static void main(String[] args) {
+        System.out.println("Hello, this is a class named after my first name sidhak.");
+    }
+}
